4b9c6b584aabeda7f5504b5cb0915d525e9418d5
Andrei Kosmylin <kosmylin-andrei@yandex.ru>
2024-09-25 20:12:39 +0300

    доработан тест AddIngredientTest и обновлен отчет jacoco

--- src/test/java/burger/AddIngredientTest.java
+++ src/test/java/burger/AddIngredientTest.java
@@ -4,27 +4,25 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import praktikum.Burger;
 import praktikum.Ingredient;
 import praktikum.IngredientType;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RunWith(MockitoJUnitRunner.class)
 public class AddIngredientTest {
-    Burger burger = new Burger();
-    Ingredient ingredient = new Ingredient(IngredientType.FILLING, "meat ball", 4.0F);
-    @Mock
-    List<Ingredient> ingredients = new ArrayList<>();
 
+    Burger burger = new Burger();
+    List<Ingredient> ingredients = burger.ingredients;
+    @Mock
+    Ingredient ingredient = new Ingredient(IngredientType.FILLING, "meat ball", 4.0F);
 
     @Test
     public void addIngredientCallsAddition() {
+        int initialSize = ingredients.size();
         burger.addIngredient(ingredient);
-        Mockito.when(ingredients.get(0)).thenReturn(ingredient);
-        Assert.assertEquals(ingredient, ingredients.get(0));
+        Assert.assertNotEquals(ingredients.size(),initialSize);
     }
 }
